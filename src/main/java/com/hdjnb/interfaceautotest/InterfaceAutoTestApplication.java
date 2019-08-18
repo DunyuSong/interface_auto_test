4c68172135245869572851372b34ed0bc501174c
857681664 <857681664@qq.com>
2019-08-18 17:12:27 +0800

    init repository

---
+++ src/main/java/com/hdjnb/interfaceautotest/InterfaceAutoTestApplication.java
@@ -0,0 +1,13 @@
+package com.hdjnb.interfaceautotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceAutoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceAutoTestApplication.class, args);
+	}
+
+}
